a1d396b27a0e64ab1cddafd88ee2f70a881b8ba3
Andrei Yastrzhembski <andrei.yastrzhembski@gmail.com>
2019-02-04 15:31:17 +0300

    refactor bin/gendiff

---
+++ src/Cli.php
@@ -0,0 +1,23 @@
+<?php
+namespace Gendiff\Cli;
+
+use function \cli\line;
+
+function run()
+{
+    //line('Generate diff');
+    $doc = <<<DOC
+Generate diff
+
+Usage:
+  gendiff (-h|--help)
+  gendiff [--format <fmt>] <firstFile> <secondFile>
+
+Options:
+  -h --help                     Show this screen
+  --format <fmt>                Report format [default: pretty]
+
+DOC;
+
+    $args = \Docopt::handle($doc);
+}
--- src/GendiffHelp.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Gendiff\GendiffHelp;
-
-use function \cli\line;
-
-function genhelp()
-{
-    line('Generate diff');
-}
