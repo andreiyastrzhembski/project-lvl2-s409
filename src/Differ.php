c639e0506bdb36fb101df56664cec8007789d2dd
Andrei Yastrzhembski <andrei.yastrzhembski@gmail.com>
2019-02-05 17:27:17 +0300

    add src/Differ.php

---
+++ src/Differ.php
@@ -0,0 +1,46 @@
+<?php
+namespace Gendiff\Differ;
+
+function genDiff(string $pathToFile1, string $pathToFile2): string
+{
+    $data1 = getData($pathToFile1);
+    $data2 = getData($pathToFile2);
+    $diff = getDiff($data1, $data2);
+
+    $result = '{' . PHP_EOL;
+    foreach ($diff as $key => $value) {
+        $result .= '  ' . $key . ': ' . $value . PHP_EOL;
+    }
+    $result .= '}' . PHP_EOL;
+
+    return $result;
+}
+
+function getData(string $pathToFile): array
+{
+    $content = file_get_contents($pathToFile);
+    return json_decode($content, true);
+}
+
+function getDiff(array $data1, array $data2): array
+{
+    $result = [];
+    foreach ($data1 as $key => $value) {
+        if (array_key_exists($key, $data2)) {
+            if ($value === $data2[$key]) {
+                $result['  ' . $key] = $value;
+            } else {
+                $result['- ' . $key] = $data1[$key];
+                $result['+ ' . $key] = $data2[$key];
+            }
+        } else {
+            $result['- ' . $key] = $value;
+        }
+    }
+    foreach ($data2 as $key => $value) {
+        if (!array_key_exists($key, $data1)) {
+            $result['+ ' . $key] = $value;
+        }
+    }
+    return $result;
+}
