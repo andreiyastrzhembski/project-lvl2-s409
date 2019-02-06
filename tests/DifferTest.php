6930cac5cf43c3f5d651dbe14c2942a48a83e1a4
Andrei Yastrzhembski <andrei.yastrzhembski@gmail.com>
2019-02-06 12:52:28 +0300

    rafactor tests/DifferTest.php

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -3,34 +3,25 @@ namespace Gendiff\Tests;
 
 use \PHPUnit\Framework\TestCase;
 
+use function Gendiff\Differ\genDiff;
 use function Gendiff\Differ\getData;
-use function Gendiff\Differ\getDiff;
+use function Gendiff\Differ\calcDiff;
 
 class DifferTest extends TestCase
 {
-    public function testGetData()
+    public function testGenDiff()
     {
-        $expected = [
-            'host' => 'hexlet.io',
-            'timeout' => 50,
-            'proxy' => '123.234.53.22'
-        ];
-        $actual = getData('tests/testData/before.json');
+        $expected = file_get_contents('tests/testData/diff_string_json');
+        $actual = genDiff('tests/testData/before.json', 'tests/testData/after.json');
         $this->assertEquals($expected, $actual);
     }
 
-    public function testGetDiff()
+    public function testCalcDiff()
     {
-        $expected = [
-            '  host' => 'hexlet.io',
-            '+ timeout' => 20,
-            '- timeout' => 50,
-            '- proxy' => '123.234.53.22',
-            '+ verbose' => true
-        ];
+        $expected = getData('tests/testData/diff.json');
         $data1 = getData('tests/testData/before.json');
         $data2 = getData('tests/testData/after.json');
-        $actual = getDiff($data1, $data2);
+        $actual = calcDiff($data1, $data2);
         $this->assertEquals($expected, $actual);
     }
 }
